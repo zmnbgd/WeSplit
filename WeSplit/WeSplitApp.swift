c4de4e58e9c13bcd8a9a8c71daef9afac4ae007d
Marko Živanović <marko.zivanovic@factoryww.com>
2024-11-27 18:57:38 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Marko Zivanovic on 27.11.24..
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
