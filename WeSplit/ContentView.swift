b4442d108d4790a803f8c2484172c21f969c95a1
Marko Živanović <marko.zivanovic@factoryww.com>
2024-11-28 18:49:05 +0100

    WeSplit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,16 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    Text("Hello world")
+                }
+                Section {
+                    Text("Hello world")
+                    Text("Hello world")
+                    Text("Hello world")
+                    Text("Hello world")
+                }
+                Section {
+                    Text("Hello world")
+                    Text("Hello world")
+                    Text("Hello world")
+                    Text("Hello world")
+                    Text("Hello world")
+                    Text("Hello world")
+                    Text("Hello world")
+                    Text("Hello world")
+                }
+            }
+            .navigationTitle("Navigation Bar")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+
+
