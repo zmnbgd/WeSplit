7081f10875fa760efb3839656e16db21997402ad
Marko Živanović <marko.zivanovic@factoryww.com>
2024-12-04 22:54:37 +0100

    Reading text from the user with TextField
    Start by adding these three properties to our ContentView struct.
    Create and store the list of possible tip.
    Adding two Sections with TextField and Text.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [0, 10, 15, 20, 25]
+    
     var body: some View {
-        Text("Hello world")
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
+        }
     }
 }
 
